d0b997315b91c5fa160f1ddfea565a89c2bc2485
Tom Chandler <tchandler48@gmail.com>
2021-01-03 13:07:52 -0600

    Update

---
+++ pmain.h
@@ -0,0 +1,13 @@
+USERID GCCCMS
+/*
+ * main.h - prototypes for main.c
+ * pmain.h - protyptes or pmain.c
+ */
+
+#ifndef _main_h_
+#define _main_h_
+
+/* void help(); */
+void banner();
+
+#endif
---
+++ rbind.h
@@ -0,0 +1,15 @@
+USERID GCCCMS
+/*  header for bind.c */
+
+#ifndef _bind_h_
+#define _bind_h_
+
+#define BIND_HEADER "bind.h"
+
+void bindfile( char *filename, char *outfile, char *dataname, char *funcname, 
+	       int manifunc );
+
+void run_bound( char *code[] );
+
+
+#endif
